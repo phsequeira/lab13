319610afc0975916110052f0727c9fc3152f98b4
phsequeira <phsequeira@yahoo.com>
2021-03-04 17:45:47 -0800

    api edit

--- src/APIutils.js
+++ src/APIutils.js
@@ -1,7 +1,7 @@
 import request from 'superagent';
 
 // API url -- different from the front end URL
-const URL = 'https://immense-ocean-97679.herokuapp.com/';
+const URL = 'https://immense-ocean-97679.herokuapp.com';
 
 export async function signUp(email, password) {
     const response = await request
